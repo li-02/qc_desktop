371057f3da9bc820837109d80dd4acf7dd79154d
kala21 <2272944319@qq.com>
2025-05-15 09:27:15 +0800

    feat: 暴露项目管理API

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,15 +1,9 @@
 // electron/preload.ts
-import { contextBridge, ipcRenderer } from 'electron';
+import {contextBridge, ipcRenderer} from 'electron';
 
 // 暴露安全的API到渲染进程
 contextBridge.exposeInMainWorld('electronAPI', {
   // 这里可以添加你需要在渲染进程中使用的函数
-  sendMessage: (channel: string, data: any) => {
-    ipcRenderer.send(channel, data);
-  },
-  receiveMessage: (channel: string, func: (...args: any[]) => void) => {
-    ipcRenderer.on(channel, (event, ...args) => func(...args));
-  },
   // 监听“新建项目”菜单项的点击事件
   onOpenCreateProjectDialog: (callback: () => void) => {
     ipcRenderer.on('open-create-project-dialog', callback);
@@ -17,5 +11,11 @@ contextBridge.exposeInMainWorld('electronAPI', {
   // 监听“导入数据”菜单项的点击事件
   onOpenImportDataDialog: (callback:()=>void) => {
     ipcRenderer.on('open-import-data-dialog', callback);
-  }
+  },
+  // 项目管理功能
+  getProjects: () => ipcRenderer.invoke('get-projects'),
+  createProject: (projectInfo: any) => ipcRenderer.invoke('create-project', projectInfo),
+  checkProjectName: (name: string) => ipcRenderer.invoke('check-project-name', name),
+  // 删除项目
+  deleteProject: (projectId: string) => ipcRenderer.invoke('delete-project', projectId)
 });

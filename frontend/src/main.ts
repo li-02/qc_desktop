552b6253360d7f8c20d1df4178cd2bbb5989d298
kala21 <2272944319@qq.com>
2025-05-20 10:55:40 +0800

    chore: 引入pureadmin/table,组件设置中文

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,20 +2,27 @@ import {createApp} from "vue";
 import "./assets/style.css";
 import App from "./App.vue";
 import ElementPlus from "element-plus";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+import plusZhCn from "plus-pro-components/es/locale/lang/zh-cn";
 import "element-plus/dist/index.css";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import "@pureadmin/table/dist/style.css";
 // 引入tailwindcss
 import "./assets/tailwind.css";
 import "animate.css";
 // 引入路由
 import router from "./router";
+import PureTable from "@pureadmin/table";
 
 const app = createApp(App);
-
+app.use(ElementPlus, {
+  locale: {...zhCn, ...plusZhCn},
+});
 // 注册所有Element Plus图标
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
 app.use(ElementPlus);
+app.use(PureTable, {locale: "zhCn"});
 app.use(router);
 app.mount("#app");

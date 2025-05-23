5074b875aea9f6a276f0bca192d6cda4e1030e70
kala21 <2272944319@qq.com>
2025-05-23 10:09:56 +0800

    refactor: 新增@shared解析路径

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,30 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: './',
-  build:{
-    outDir: 'dist',
+  base: "./",
+  build: {
+    outDir: "dist",
     emptyOutDir: true,
     sourcemap: true,
   },
-  resolve:{
-    alias:{
-      '@': path.resolve(__dirname, './src'), 
-    }
-  }
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@shared": path.resolve(__dirname, "../shared"),
+    },
+  },
+  server: {
+    fs: {
+      allow: [".."],
+    },
+  },
+  define: {
+    __VUE_OPTIONS_AIP__: true,
+    __VUE_PROD_DEVTOOLS__: false,
+  },
+});
